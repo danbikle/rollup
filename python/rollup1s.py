59d8c06972def0c3f4117121c7a3941c4182b234
Dan Bikle <danbikle@outlook.com>
2017-11-12 14:04:51 -0800

    more

--- python/rollup1s.py
+++ python/rollup1s.py
@@ -22,8 +22,11 @@ for fn_s in sorted(fn_l):
     ts1s_sr        = fx0_df.ts.str.slice(0,17)
     fx0_df['ts1s'] = ts1s_sr
     # I should group-by ts1s and avg bid, ask
-    fx1_df = fx0_df.copy()[['ts1s','bid']]
-    fx2_df = fx1_df.groupby('ts1s').bid.mean()
+    fx1_df = fx0_df.copy()[['ts1s','bid','ask']]
+    bid_sr = fx1_df.groupby('ts1s').bid.mean()
+    ask_sr = fx1_df.groupby('ts1s').ask.mean()
+    # I should create a DF from bid_sr, ask_sr with ts1s as the index:
+    fx2_df = pd.DataFrame({'bid':bid_sr, 'ask':ask_sr})
     type(fx2_df)
     fx2_df.head()
     'bye'
