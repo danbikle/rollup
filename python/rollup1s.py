1db3f1c72d2f254ff37330a97f9753d7b4f1548c
Dan Bikle <danbikle@outlook.com>
2017-11-13 12:29:43 -0800

    more

--- python/rollup1s.py
+++ python/rollup1s.py
@@ -7,12 +7,10 @@ The results should get written to ../data/csv1s/
 
 Demo:
 ~/anaconda3/bin/python rollup1s.py
-
 """
 
 import glob
 import pandas as pd
-import pdb
 import os
 
 # I should ensure that the output folder exists:
@@ -24,13 +22,14 @@ for fn_s in sorted(fn_l):
     fx0_df = pd.read_csv(fn_s, names=['pair','ts','bid','ask'])
     ts1s_sr        = fx0_df.ts.str.slice(0,17)
     fx0_df['ts1s'] = ts1s_sr
-    # I should group-by ts1s and avg bid, ask
+    # I should group-by ts1s and find mean of ask and bid:
     ask_sr = fx0_df.groupby('ts1s').ask.mean()
     bid_sr = fx0_df.groupby('ts1s').bid.mean()
     # I should create a DF from bid_sr, ask_sr with ts1s as the index:
     fx1_df = pd.DataFrame({'bid':bid_sr, 'ask':ask_sr})
-    # I should write it to CSV:
+    # I should create a name for the output csv file:
     csvn_s = fn_s.replace('zip','csv').replace('/csv/','/csv1s/')
+    # I should write it to CSV and compress it:
     fx1_df.to_csv(csvn_s+'.bz2',float_format='%4.6f',compression='bz2')
     print('Wrote: ',csvn_s+'.bz2')
     # I can inspect output with python:
@@ -43,5 +42,4 @@ for fn_s in sorted(fn_l):
     del(ts1s_sr)
     del(ask_sr)
     del(bid_sr)
-    'bye'
 'bye'
